77797d3624d061a891be3ac5cfa012628c1b453a
Eddie Haddican <edwardhaddican@gmail.com>
2021-05-23 18:12:42 -0400

    seeding spells script finshed and working

--- script/fetchSpells.js
+++ script/fetchSpells.js
@@ -1,68 +1,97 @@
-// import React, { useState, useEffect, useCallback } from "react";
-// import Axios from "axios";
+const axios = require("axios");
+const { Spell } = require("../server/db/models");
 
-// const fetchAllSpellsFromAPI = useCallback(async () => {
-//   const [allSpells, setAllSpells] = useState([]);
-//   const [classSpells, setClassSpells] = useState({});
-//   const [error, setError] = useState("");
-//   try {
-//     let url = "https://www.dnd5eapi.co/api/spells";
+const fetchSingleSpell = async (url) => {
+  try {
+    let baseUrl = "https://www.dnd5eapi.co";
 
-//     const response = await Axios.get(url);
+    const response = await axios.get(baseUrl + url);
 
-//     if (response.data.Error) {
-//       setError(response.data.Error);
-//     } else {
-//       setError("");
-//       setAllSpells(response.data.results);
-//       setClassSpells(response.data.results);
-//     }
-//   } catch (error) {
-//     console.log("You have an error", error);
-//   }
-// }, [setAllSpells, setClassSpells, setError]);
+    if (response.data.Error) {
+      console.log("you have an error: ", response.data.Error);
+    } else {
+      // console.log(response.data)
+      if (response.data.index === "fireball") {
+        console.log(response.data);
+      }
 
+      //put into database
+      const data = response.data;
 
-// let spellIndex = props.match.params.spellIndex;
+      Spell.create({
+        name: data.name,
+        index: data.index,
+        level: data.level,
+        school: data.school.index,
+        classes: data.classes.map(function (currentClass) {
+          return currentClass.index;
+        }),
+        url: data.url,
+        description: data.desc,
+        area_of_effect_size: data.area_of_effect
+          ? data.area_of_effect.size
+          : null,
+        area_of_effect_type: data.area_of_effect
+          ? data.area_of_effect.type
+          : null,
+        casting_time: data.casting_time,
+        components: data.components,
+        material: data.material,
+        concentration: data.concentration,
+        damage_at_slot_level: JSON.stringify(data.damage_at_slot_level),
+        dc_type: data.dc ? data.dc.dc_type.index : null,
+        dc_success: data.dc ? data.dc.dc_success : null,
+        damage_type: data.damage
+          ? data.damage.damage_type
+            ? data.damage.damage_type.index
+            : null
+          : null,
+        duration: data.duration,
+        higher_level: data.higher_level,
+        range: data.range,
+        ritual: data.ritual,
+        subclasses: data.classes.map(function (currentClass) {
+          return currentClass.index;
+        }),
+      });
+    }
+  } catch (error) {
+    console.log("You have an error", error);
+  }
+};
 
-// const fetchSingleSpell = useCallback(async () => {
-//   try {
-//     let url = "https://www.dnd5eapi.co/api/spells/";
+const allSpellList = async () => {
+  try {
+    let url = "https://www.dnd5eapi.co/api/spells";
 
-//     const response = await Axios.get(url + spellIndex);
+    const response = await axios.get(url);
 
-//     if (response.data.Error) {
-//       setError(response.data.Error);
-//     } else {
-//       setError("");
-//       setCurrentSpell(response.data);
+    if (response.data.Error) {
+      console.log("you have an error: ", response.data.Error);
+    } else {
+      //add to my database
+      // console.log(response.data.results)
+      const promiseArray = response.data.results.map((spell) => {
+        return fetchSingleSpell(spell.url);
+      });
 
-//       if (response.data.damage && response.data.damage.damage_at_slot_level) {
-//         setDamageObj(response.data.damage.damage_at_slot_level);
-//         setDamageObjKeys(
-//           Object.keys(response.data.damage.damage_at_slot_level)
-//         );
-//       }
-//     }
-//   } catch (error) {
-//     console.log("You have an error", error);
-//   }
-// }, [spellIndex]);
+      await Promise.all(promiseArray);
 
+      console.log("all done");
+    }
+  } catch (error) {
+    console.log("You have an error", error);
+  }
+};
 
-
-
-
-
-
-// const classSpellList = useCallback(async () => {
+// const classSpellList = async () => {
 //   try {
 //     let url = "https://www.dnd5eapi.co";
 
-//     const response = await Axios.get(url + "/api/classes");
+//     const response = await axios.get(url + "/api/classes");
 
 //     const promises = response.data.results.map((element) => {
-//       return Axios.get(`${url}${element.url}/spells`);
+//       return axios.get(`${url}${element.url}/spells`);
 //     });
 
 //     const classSpellResults = await Promise.all(promises);
@@ -72,51 +101,12 @@
 //       result[playerClass] = element.data.results;
 //       return result;
 //     }, {});
-//     setClassSpells(classSpells);
+//     // console.log(classSpells, 'class spells in new script')
 //   } catch (err) {
-//     setError(err);
 //     console.log("You have an error", error);
 //   }
-// }, [setClassSpells, setError]);
-
-// useEffect(() => {
-//   allSpellList();
-//   classSpellList();
-// }, [allSpellList, classSpellList]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const makeFirstLetterCap = (currentSelectedClass) => {
-//   const arrayOfLetters = currentSelectedClass.split("");
-//   let firstLetter = arrayOfLetters.shift();
-//   firstLetter = firstLetter.toUpperCase();
-//   arrayOfLetters.unshift(firstLetter);
-//   let fullWord = arrayOfLetters.join("");
-//   return fullWord;
 // };
 
-// // const searchForSpellByLevel = (spellLevel) => {
-// //   if(spellLevel){
-
-// //   }
-
-// // };
-
-// export default fetchAllSpellsFromAPI;
-
-
-
-
-
-
-// this is part of the url to search by schools: "/api/magic-schools/evocation
+Spell.sync({ force: true });
+allSpellList();
+// classSpellList();
